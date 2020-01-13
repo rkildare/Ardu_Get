7ae7ee760e3b827ac192e3615cbda243ba0acfd4
rkildare <55807922+rkildare@users.noreply.github.com>
2020-01-13 13:07:19 -0800

    Add project files.

---
+++ quickmake/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace quickmake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cmbPort = new System.Windows.Forms.ComboBox();
+            this.cmbBaud = new System.Windows.Forms.ComboBox();
+            this.chkSave = new System.Windows.Forms.CheckBox();
+            this.txtLoc = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.rtxtOut = new System.Windows.Forms.RichTextBox();
+            this.chartOut = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.saveDia = new System.Windows.Forms.SaveFileDialog();
+            this.btnStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chartOut)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbPort
+            // 
+            this.cmbPort.FormattingEnabled = true;
+            this.cmbPort.Location = new System.Drawing.Point(13, 13);
+            this.cmbPort.Name = "cmbPort";
+            this.cmbPort.Size = new System.Drawing.Size(121, 21);
+            this.cmbPort.TabIndex = 0;
+            this.cmbPort.Text = "Port";
+            // 
+            // cmbBaud
+            // 
+            this.cmbBaud.FormattingEnabled = true;
+            this.cmbBaud.Items.AddRange(new object[] {
+            "110",
+            "300",
+            "600",
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "14400",
+            "19200",
+            "38400"});
+            this.cmbBaud.Location = new System.Drawing.Point(12, 40);
+            this.cmbBaud.Name = "cmbBaud";
+            this.cmbBaud.Size = new System.Drawing.Size(121, 21);
+            this.cmbBaud.TabIndex = 1;
+            this.cmbBaud.Text = "Baud rate";
+            // 
+            // chkSave
+            // 
+            this.chkSave.AutoSize = true;
+            this.chkSave.Location = new System.Drawing.Point(13, 68);
+            this.chkSave.Name = "chkSave";
+            this.chkSave.Size = new System.Drawing.Size(83, 17);
+            this.chkSave.TabIndex = 2;
+            this.chkSave.Text = "Save Data?";
+            this.chkSave.UseVisualStyleBackColor = true;
+            // 
+            // txtLoc
+            // 
+            this.txtLoc.Location = new System.Drawing.Point(13, 92);
+            this.txtLoc.Name = "txtLoc";
+            this.txtLoc.Size = new System.Drawing.Size(121, 20);
+            this.txtLoc.TabIndex = 3;
+            this.txtLoc.Text = "Location";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(13, 119);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(120, 25);
+            this.btnBrowse.TabIndex = 4;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.BtnBrowse_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 151);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(57, 45);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // rtxtOut
+            // 
+            this.rtxtOut.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtxtOut.Location = new System.Drawing.Point(141, 13);
+            this.rtxtOut.Name = "rtxtOut";
+            this.rtxtOut.Size = new System.Drawing.Size(450, 183);
+            this.rtxtOut.TabIndex = 6;
+            this.rtxtOut.Text = "";
+            // 
+            // chartOut
+            // 
+            this.chartOut.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.chartOut.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartOut.Legends.Add(legend1);
+            this.chartOut.Location = new System.Drawing.Point(13, 203);
+            this.chartOut.Name = "chartOut";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Legend = "Legend1";
+            series3.Name = "Series3";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Legend = "Legend1";
+            series4.Name = "Series4";
+            this.chartOut.Series.Add(series1);
+            this.chartOut.Series.Add(series2);
+            this.chartOut.Series.Add(series3);
+            this.chartOut.Series.Add(series4);
+            this.chartOut.Size = new System.Drawing.Size(578, 280);
+            this.chartOut.TabIndex = 7;
+            this.chartOut.Text = "chart1";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(75, 152);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(57, 45);
+            this.btnStop.TabIndex = 8;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.BtnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 495);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.chartOut);
+            this.Controls.Add(this.rtxtOut);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtLoc);
+            this.Controls.Add(this.chkSave);
+            this.Controls.Add(this.cmbBaud);
+            this.Controls.Add(this.cmbPort);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Arduino Data Getter V1.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chartOut)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbPort;
+        private System.Windows.Forms.ComboBox cmbBaud;
+        private System.Windows.Forms.CheckBox chkSave;
+        private System.Windows.Forms.TextBox txtLoc;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.RichTextBox rtxtOut;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartOut;
+        private System.Windows.Forms.SaveFileDialog saveDia;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ quickmake/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace quickmake
+{
+    public partial class Form1 : Form
+    {
+        public delegate void AddDataDelegate(String myString);
+        public AddDataDelegate myDelegate;
+
+        SerialPort port = new SerialPort();
+        public Form1()
+        {
+            InitializeComponent();
+            cmbPort.Items.AddRange(SerialPort.GetPortNames());
+        }
+
+        private SerialPort Make_Port()
+        {
+            SerialPort serPrt = new SerialPort(cmbPort.Text, Convert.ToInt32(cmbBaud.Text));
+            return serPrt;
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            port.PortName = cmbPort.Text; 
+            port.BaudRate = Convert.ToInt32(cmbBaud.Text);
+            port.DataReceived += new SerialDataReceivedEventHandler(DataRecievedHandler);
+            port.Open();
+            port.DiscardInBuffer();
+            port.DiscardOutBuffer();
+        }
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            port.Close();
+            port.Dispose();
+        }
+        public void PrintSave(String read)
+        {
+            if (read != "")
+            {
+                bool nope = false;
+                int loop = 0;
+                double testdbl = 0;
+                try
+                {
+                    rtxtOut.AppendText(read + "\n");
+                    string val = read;
+                    string[] strarr = val.Split(',');
+                    while (loop < strarr.Length)
+                    {
+                        if (double.TryParse(strarr[loop], out testdbl) == false)
+                        {
+                            nope = true;
+                        }
+                        loop = loop + 1;
+                    }
+                    if (nope == false)
+                    {
+                        double[] graphvals = Array.ConvertAll<string, double>(strarr, double.Parse);
+                        if (graphvals.Length >= 1) { chartOut.Series["Series1"].Points.AddY(graphvals[0]); }
+                        if (graphvals.Length >= 2) { chartOut.Series["Series2"].Points.AddY(graphvals[1]); }
+                        if (graphvals.Length >= 3) { chartOut.Series["Series3"].Points.AddY(graphvals[2]); }
+                        if (graphvals.Length >= 4) { chartOut.Series["Series4"].Points.AddY(graphvals[3]); }
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+
+            if (chkSave.Checked)
+            {
+                try
+                {
+                    using (System.IO.StreamWriter sw = System.IO.File.AppendText(txtLoc.Text))
+                    {
+                        sw.Write(read + "\n");
+                    }
+                }
+                catch (System.IO.DirectoryNotFoundException)
+                {
+                    rtxtOut.AppendText("Error... Invalid file location. Please check and try again!!!\n");
+                }
+            }
+        }
+
+        private void BtnBrowse_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog Save1 = new SaveFileDialog();
+            Save1.Filter = "Text file (*.txt)|*.txt|CSV File (*.csv)|*.csv|All Files (*.*)|*.*";
+            Save1.DefaultExt = ".txt";
+            Save1.OverwritePrompt = false;
+            Save1.AddExtension = true;
+            Save1.ShowDialog();
+            txtLoc.Text = Save1.FileName;
+        }
+
+        private void DataRecievedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            try
+            {
+                
+                string data = sp.ReadLine();
+                Console.WriteLine("recieved data:" + data);
+                Invoke(this.myDelegate, new Object[] { data });
+                
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.myDelegate = new AddDataDelegate(PrintSave);
+        }
+    }
+}
